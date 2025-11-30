4861c358c2f9a5e3bb8caafc2604eb04b8457796
barbaraconejero <bconejero26@gmail.com>
2025-11-29 23:21:17 -0300

    actualizacion

--- assets/javascripts/javascript1.js
+++ assets/javascripts/javascript1.js
@@ -2,5 +2,23 @@ function scrollToNextSection() {
     const next = document.getElementById("seccion2");
     next.scrollIntoView({ behavior: "smooth" });
 }
+// efecto rebote al sacar el mouse de cada sticker
+
+document.querySelectorAll('.sticker').forEach(sticker => {
+
+    // Al salir el mouse del sticker
+    sticker.addEventListener('mouseleave', () => {
+
+        // agregamos la clase temporal de rebote
+        sticker.classList.add('bouncing');
+
+        // quitamos la clase después de la animación
+        setTimeout(() => {
+            sticker.classList.remove('bouncing');
+        }, 450); // duración exacta del bounceExit
+    });
+
+});
+
 
 
